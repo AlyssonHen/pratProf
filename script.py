440d391f9b78bcf5cc4c4efbabe8690e3cad04ef
AlyssonHen <alysson.henrique.souza05@aluno.ifce.edu.br>
2020-08-25 01:18:31 +0000

    atividade pratica

--- script.py
+++ script.py
@@ -1,1 +1,18 @@
+import math
 print('teste script')
+
+
+def hipot(triangulo):
+    cA = triangulo['catetoA']
+    cO = triangulo['catetoO']
+    hipot = (cA * cA) + (cO * cO)
+    hipot = math.sqrt(hipot)
+    hipot = round(hipot, 2)
+    return hipot
+
+medidas = {
+    'catetoA': 10,
+    'catetoO': 32
+}
+
+print(hipot(medidas))
